60c72bd488a6b2c267f0ba2bda90a5f8cc775bc3
Patryk Michalak <Patryk 'MrDeun' Michalak>
2025-06-14 02:01:33 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,79 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "TodoAPI";
+    config.Title = "TodoAPI v1";
+    config.Version = "v1";
+});
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "TodoAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
+
+app.MapGet("/todoitems", async (TodoContext db) =>
+    await db.TodoItems.ToListAsync());
+
+app.MapGet("/todoitems/complete", async (TodoContext db) =>
+    await db.TodoItems.Where(t => t.IsComplete).ToListAsync());
+
+app.MapGet("/todoitems/{id}", async (int id, TodoContext db) =>
+    await db.TodoItems.FindAsync(id)
+        is TodoItem todo
+            ? Results.Ok(todo)
+            : Results.NotFound());
+
+app.MapPost("/todoitems", async (TodoItem todo, TodoContext db) =>
+{
+    db.TodoItems.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
+
+app.MapPut("/todoitems/{id}", async (int id, TodoItem inputTodo, TodoContext db) =>
+{
+    var todo = await db.TodoItems.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/todoitems/{id}", async (int id, TodoContext db) =>
+{
+    if (await db.TodoItems.FindAsync(id) is TodoItem todo)
+    {
+        db.TodoItems.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
+
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+
+
+app.Run();
